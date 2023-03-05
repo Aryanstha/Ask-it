9aceb49aacc17afa5de6b4d4d74a09c075044274
Aryan Shrestha <67673221+Aryanstha@users.noreply.github.com>
2023-03-05 18:27:46 +0545

    Update speech.js

--- client/speech.js
+++ client/speech.js
@@ -1,41 +1,60 @@
-let recognition = null;
-      let isListening = false;
+const texts = document.querySelector(".texts");
 
-      function startSpeech() {
-        if (!recognition) {
-          recognition = new webkitSpeechRecognition();
-          recognition.continuous = true;
-          recognition.interimResults = true;
-          recognition.lang = 'en-US';
-          recognition.onresult = handleSpeechResult;
-        }
-        recognition.start();
-        isListening = true;
-      }
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-      function stopSpeech() {
-        if (recognition) {
-          recognition.stop();
-        }
-        isListening = false;
-      }
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
 
-      function handleSpeechResult(event) {
-        let result = '';
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-          result += event.results[i][0].transcript;
-        }
-        document.getElementById('result').textContent = result;
-      }
+let p = document.createElement("p");
 
-      document.addEventListener('keydown', event => {
-        if (event.key === ' ' && !isListening) {
-          startSpeech();
-        }
-      });
+recognition.addEventListener("result", (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
 
-      document.addEventListener('keyup', event => {
-        if (event.key === ' ' && isListening) {
-          stopSpeech();
-        }
-      });
+  p.innerText = text;
+  if (e.results[0].isFinal) {
+    if (text.includes("how are you")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am fine";
+      texts.appendChild(p);
+    }
+    if (
+      text.includes("what's your name") ||
+      text.includes("what is your name")
+    ) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "I am Js";
+      texts.appendChild(p);
+    }
+    if (text.includes("open YouTube")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening youtube channel";
+      texts.appendChild(p);
+      console.log("opening youtube");
+      window.open("https://www.youtube.com/");
+    }
+    if (text.includes("open arniko lab page")) {
+      p = document.createElement("p");
+      p.classList.add("replay");
+      p.innerText = "opening page";
+      texts.appendChild(p);
+      console.log("opening page");
+      window.open("https://www.facebook.com/arnikointernationalcollege");
+
+    }
+    p = document.createElement("p");
+  }
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+recognition.start();
